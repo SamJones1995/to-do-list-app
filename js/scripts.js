879679cd50f947ddfcb32eba32bae072d5e1626e
SamJones1995 <88803371+SamJones1995@users.noreply.github.com>
2022-05-23 02:42:00 -0400

    Fix: display 'X' to the right

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,28 @@
+function newItem() {
+    //list set up to add list item using Add button
+    let li = $("<li></li>");
+    let inputValue = $("#input").val();
+    li.append(inputValue);
 
-function newItem(){
-//list set up to add list item using Add button
-	let li = $('<li></li>');
-	  let inputValue = $('#input').val();
-	  li.append(inputValue);
+    if (inputValue === "") {
+        alert("You must write something!");
+    } else {
+        $("#list").append(li);
+    }
+    //crossout function on double click
+    function crossOut() {
+        li.toggleClass("strike");
+    }
 
-	  if (inputValue === '') {
-	    alert("You must write something!");
-	  } else {
-	    $('#list').append(li);
-	  }
-//crossout function on double click
-	 function crossOut() {
-	    li.toggleClass('strike');
-	  }
+    li.on("dblclick", crossOut);
+    //delete button at the end of each item marked by X
+    let crossOutButton = $("<crossOutButton></crossOutButton>");
+    crossOutButton.append(document.createTextNode("X"));
+    li.append(crossOutButton);
 
-	  li.on('dblclick',  crossOut);
-//delete button at the end of each item marked by X
-	  let deleteButton = $('<deletebutton></deletebutton>');
-	  deleteButton.append(document.createTextNode('X'));
-	  li.append(deleteButton);
-
-	  function deleteListItem() {
-	  	li.addClass('delete');
-	  }
-
-	  deleteButton.on('click', deleteListItem);
-//allows the user to drag list items around
-	  $('#list').sortable()
-
+    crossOutButton.on("click", function () {
+        $(this).parent().remove();
+    });
+    //allows the user to drag list items around
+    $("#list").sortable();
 }
-
